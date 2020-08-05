6585cda859e11f2f5e6794a93aa30dbb6e4d57ef
Deena1999 <53299197+Deena1999@users.noreply.github.com>
2020-08-05 18:45:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,4 +19,29 @@ app.post('/post_data',async(req,res,next) => {
     return res.json("ok");
 });
 
+app.patch('/patch_data',async(req,res,next) => {
+    const update_name=req.body.name;
+    const email=req.body.email;
+    const [data] =await db.execute('select email from registeration where email=?',[email]);
+    if(data.length===1)
+    {
+        await db.execute('update registeration set name=? where email=?',[update_name,email]);
+        return res.json({status:"ok"});
+    }
+    return res.json("User with email does not exist");
+})
+
+app.delete('/delete_data',async(req,res,next) => {
+    const email=req.body.email;
+    const [check]=await db.execute('select email from registeration where email=?',[email]);
+    if(check.length===1)
+    {
+        await db.execute('delete from registeration where email=?',[email]);
+        return res.json({status:"ok"});
+    }
+    return res.json("User cannot be deleted");
+});
+
+
+
 app.listen(9000);
