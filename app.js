9afe6b08c9d2869e3cf3857c0baecbddc515f402
Deena1999 <53299197+Deena1999@users.noreply.github.com>
2020-08-06 18:20:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const bodyparser=require('body-parser');
 const db=require('./model/databse');
+const nodemailer=require('nodemailer');
+const path=require('path');
 
 const app=express();
 
@@ -11,11 +13,43 @@ app.get('/gettingrequest',(req,res,next) => {
     res.json("Getting the data from browser");
 });
 
+
+
 app.post('/post_data',async(req,res,next) => {
     const name=req.body.name;
     const email=req.body.email;
+    const password=req.body.password;
+    const mobile_number=req.body.number;
     console.log(name);
-    await db.execute('insert into registeration (name,email) values(?,?)',[name,email]);
+    await db.execute('insert into registeration (name,email,password,number) values(?,?,?,?)',[name,email,password,mobile_number]);
+    
+    let transport=nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:'nodeproject712@gmail.com',
+            pass:'Nodeproject@1'
+        }
+    });
+    
+
+    let mail={
+        from:"nodeproject712@gmail.com",
+        to:`${email}`,
+        subject:'#Regarding Internship Registeration',
+        html:'<h2>You have been successfully registered for our internship series</h2>'
+    };
+
+    transport.sendMail(mail,(error,info) => {
+        if(error)
+        {
+            console.log(error);
+        }
+        else{
+            console.log(info.response);
+        }
+    });
+
+    
     return res.json("ok");
 });
 
@@ -41,6 +75,11 @@ app.delete('/delete_data',async(req,res,next) => {
     }
     return res.json("User cannot be deleted");
 });
+
+app.get('/file',(req,res,next) => {
+    console.log(path.join(__dirname));
+    res.sendFile(path.join(__dirname,"app.js"));
+})
 
 
 
